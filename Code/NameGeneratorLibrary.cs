a92ecbf8ce1fbd67b4f5238961ef48ea5ac68408
inmny <211240032@smail.nju.edu.cn>
2023-06-15 19:39:31 +0800

    实现加载其他模组词库和命名规则

--- Code/NameGeneratorLibrary.cs
+++ Code/NameGeneratorLibrary.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NCMS;
 using Newtonsoft;
+using UnityEngine;
 
 namespace Chinese_Name
 {
@@ -32,27 +34,30 @@ namespace Chinese_Name
             if (Directory.Exists(Main.path_to_name_generators)) return;
             /* If unzip fails */
             Main.curr_path_to_name_generators = Main.path_to_tmp_name_generators;
-            if (Directory.Exists(Main.path_to_tmp_name_generators)) return;
+            if (Directory.Exists(Main.path_to_tmp_name_generators))
+            {
+                Directory.Delete(Main.path_to_tmp_name_generators, true);
+            }
 
             Directory.CreateDirectory(Main.path_to_tmp_name_generators);
             ZipFile.ExtractToDirectory(Main.zipped_name_generators_path, Main.path_to_tmp_name_generators);
         }
-        public void load_default_name_generators()
+        private void load_name_generators_from(string path)
         {
-            string[] files = System.IO.Directory.GetFiles(Main.curr_path_to_name_generators);
-            
-            Main.warn($"NAME GENERATORS at ({Main.curr_path_to_name_generators}):"+files.Length.ToString());
+            string[] files = System.IO.Directory.GetFiles(path);
+
+            Main.warn($"NAME GENERATORS at ({path}):" + files.Length.ToString());
             foreach (string file_path in files)
             {
                 //Main.warn(file_path);
                 Dictionary<string, NameGenerator> generators_in_file = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, NameGenerator>>(File.ReadAllText(file_path));
                 if (generators_in_file == null) continue;
-                foreach(string key in generators_in_file.Keys)
+                foreach (string key in generators_in_file.Keys)
                 {
                     generators[key] = generators_in_file[key];
                     generators[key].total_weight = 0;
                     generators[key].generate_func = generate_funcs[generators[key].generate_func_id];
-                    foreach(NameGenerateTemplate template in generators[key].templates)
+                    foreach (NameGenerateTemplate template in generators[key].templates)
                     {
                         template.finish();
                         generators[key].total_weight += template.weight;
@@ -60,9 +65,33 @@ namespace Chinese_Name
                 }
             }
         }
+        public void load_default_name_generators()
+        {
+            load_name_generators_from(Main.curr_path_to_name_generators);
+        }
         public void load_mods_name_generators()
         {
+            if (!Directory.Exists(Main.path_to_tmp_name_generators))
+            {
+                Directory.CreateDirectory(Main.path_to_tmp_name_generators);
+            }
+            foreach (NCMod mod in NCMS.ModLoader.Mods)
+            {
+                if (mod.name == Mod.Info.Name) continue;
 
+                string folder_name = Hash128.Compute(mod.name).ToString();
+                    
+                string path = Path.Combine(mod.path, "name_generators.zip");
+                string target_path = Path.Combine(Main.path_to_tmp_name_generators, folder_name);
+
+                if (File.Exists(path))
+                {
+                    if (!Directory.Exists(target_path)) Directory.CreateDirectory(target_path);
+                    ZipFile.ExtractToDirectory(path, target_path);
+
+                    load_name_generators_from(target_path);
+                }
+            }
         }
         public NameGenerator get(string id)
         {
--- Code/WordLibraries.cs
+++ Code/WordLibraries.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.IO.Compression;
 using UnityEngine;
+using NCMS;
 
 namespace Chinese_Name
 {
@@ -25,25 +26,51 @@ namespace Chinese_Name
 
             /* If unzip fails */
             Main.curr_path_to_word_libraries = Main.path_to_tmp_word_libraries;
-            if (Directory.Exists(Main.path_to_tmp_word_libraries)) return;
+            if (Directory.Exists(Main.path_to_tmp_word_libraries)) 
+            {
+                Directory.Delete(Main.path_to_tmp_word_libraries, true);
+            }
 
             Directory.CreateDirectory(Main.path_to_tmp_word_libraries);
             ZipFile.ExtractToDirectory(Main.zipped_word_libraries_path, Main.path_to_tmp_word_libraries);
         }
         public void load_default_word_libraries()
         {
-            string[] files = System.IO.Directory.GetFiles(Main.curr_path_to_word_libraries);
-            Main.warn($"WORD LIBRARIES at ({Main.curr_path_to_word_libraries}):"+files.Length.ToString());
+            load_word_libraries_from(Main.curr_path_to_word_libraries);
+        }
+        private void load_word_libraries_from(string path)
+        {
+            string[] files = System.IO.Directory.GetFiles(path);
+            Main.warn($"WORD LIBRARIES at ({path}):" + files.Length.ToString());
             foreach (string file_path in files)
             {
                 //Main.warn(file_path);
                 string[] paths = file_path.Replace("\\", "/").Split('/');
-                libraries[paths[paths.Length - 1].Replace(".txt","")] = new WordLibrary(load_list_str(file_path));
+                libraries[paths[paths.Length - 1].Replace(".txt", "")] = new WordLibrary(load_list_str(file_path));
             }
         }
         public void load_mods_word_libraries()
         {
-            
+            if(!Directory.Exists(Main.path_to_tmp_word_libraries))
+            {
+                Directory.CreateDirectory(Main.path_to_tmp_word_libraries);
+            }
+            foreach(NCMod mod in NCMS.ModLoader.Mods)
+            {
+                if (mod.name == Mod.Info.Name) continue;
+
+                string folder_name = Hash128.Compute(mod.name).ToString();
+                string path = Path.Combine(mod.path, "word_libraries.zip");
+                string target_path = Path.Combine(Main.path_to_tmp_word_libraries, folder_name);
+
+                if (File.Exists(path))
+                {
+                    if(!Directory.Exists(target_path)) Directory.CreateDirectory(target_path);
+                    ZipFile.ExtractToDirectory(path, target_path);
+
+                    load_word_libraries_from(target_path);
+                }
+            }
         }
         public WordLibrary get(string id)
         {
