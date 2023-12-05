53bcf25d982c5738eb812c5793da87eebffa5314
inmny <1342885270@qq.com>
2023-12-05 13:45:13 +0800

    Bugfix: 防止防守方首都不存在

--- Code/ParameterGetters.cs
+++ Code/ParameterGetters.cs
@@ -47,31 +47,35 @@ public static class ParameterGetters
         {
             { "default", default_item_parameter_getter }
         };
+
     private static Dictionary<Type, Dictionary<string, Delegate>> custom_parameter_getters = new();
+
     [Hotfixable]
     private static void default_actor_parameter_getter(Actor pActor, Dictionary<string, string> pParameters)
     {
-        
     }
+
     [Hotfixable]
     private static void default_city_parameter_getter(City pCity, Dictionary<string, string> pParameters)
     {
-        
     }
+
     [Hotfixable]
     private static void default_kingdom_parameter_getter(Kingdom pKingdom, Dictionary<string, string> pParameters)
     {
-        
     }
+
     [Hotfixable]
     private static void default_culture_parameter_getter(Culture pCulture, Dictionary<string, string> pParameters)
     {
-        
     }
+
     [Hotfixable]
     private static void default_clan_parameter_getter(Clan pClan, Actor pActor, Dictionary<string, string> pParameters)
     {
-        pParameters["founder_home"] = string.IsNullOrEmpty(pClan.data.founder_home) ? pClan.data.founder_kingdom : pClan.data.founder_home;
+        pParameters["founder_home"] = string.IsNullOrEmpty(pClan.data.founder_home)
+            ? pClan.data.founder_kingdom
+            : pClan.data.founder_home;
 
         if (pActor == null)
         {
@@ -91,11 +95,12 @@ public static class ParameterGetters
             pParameters["founder_family_name"] = family_name;
         }
     }
+
     [Hotfixable]
     private static void default_alliance_parameter_getter(Alliance pAlliance, Dictionary<string, string> pParameters)
     {
-        
     }
+
     [Hotfixable]
     private static void default_war_parameter_getter(War pWar, Dictionary<string, string> pParameters)
     {
@@ -103,98 +108,119 @@ public static class ParameterGetters
         pParameters["defender"] = pWar.main_defender?.data.name;
         pParameters["attacker_short"] = pWar.main_attacker.data.name[0].ToString();
         pParameters["defender_short"] = pWar.main_defender?.data.name[0].ToString();
-        pParameters["defender_capital"] = pWar.main_defender?.capital.data.name;
+        pParameters["defender_capital"] = pWar.main_defender?.capital?.data.name;
         ModClass.LogInfo("war: " + pWar.getAsset().name_template);
         ModClass.LogInfo("attacker: " + pParameters["attacker"]);
         ModClass.LogInfo("defender: " + pParameters["defender"]);
         ModClass.LogInfo("defender_capital: " + pParameters["defender_capital"]);
     }
+
     [Hotfixable]
-    private static void default_item_parameter_getter(ItemData pItemData, ItemAsset pItemAsset, Actor pActor, Dictionary<string, string> pParameters)
+    private static void default_item_parameter_getter(ItemData pItemData, ItemAsset pItemAsset, Actor pActor,
+        Dictionary<string, string> pParameters)
     {
-        
     }
+
     public static Action<Actor, Dictionary<string, string>> GetActorParameterGetter(string pName)
     {
         if (actor_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return actor_parameter_getters["default"];
     }
+
     public static Action<City, Dictionary<string, string>> GetCityParameterGetter(string pName)
     {
         if (city_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return city_parameter_getters["default"];
     }
+
     public static Action<Kingdom, Dictionary<string, string>> GetKingdomParameterGetter(string pName)
     {
         if (kingdom_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return kingdom_parameter_getters["default"];
     }
+
     public static Action<Culture, Dictionary<string, string>> GetCultureParameterGetter(string pName)
     {
         if (culture_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return culture_parameter_getters["default"];
     }
+
     public static Action<Clan, Actor, Dictionary<string, string>> GetClanParameterGetter(string pName)
     {
         if (clan_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return clan_parameter_getters["default"];
     }
+
     public static Action<Alliance, Dictionary<string, string>> GetAllianceParameterGetter(string pName)
     {
         if (alliance_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return alliance_parameter_getters["default"];
     }
+
     public static Action<War, Dictionary<string, string>> GetWarParameterGetter(string pName)
     {
         if (war_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return war_parameter_getters["default"];
     }
+
     public static Action<ItemData, ItemAsset, Actor, Dictionary<string, string>> GetItemParameterGetter(string pName)
     {
         if (item_parameter_getters.TryGetValue(pName, out var getter)) return getter;
         return item_parameter_getters["default"];
     }
+
     public static T GetCustomParameterGetter<T>(string pName) where T : Delegate
     {
-        if(custom_parameter_getters.TryGetValue(typeof(T), out var getters))
+        if (custom_parameter_getters.TryGetValue(typeof(T), out var getters))
         {
             if (getters.TryGetValue(pName, out var getter)) return (T)getter;
             return getters["default"] as T;
         }
+
         return null;
     }
+
     public static void PutActorParameterGetter(string pName, Action<Actor, Dictionary<string, string>> pGetter)
     {
         actor_parameter_getters[pName] = pGetter;
     }
+
     public static void PutCityParameterGetter(string pName, Action<City, Dictionary<string, string>> pGetter)
     {
         city_parameter_getters[pName] = pGetter;
     }
+
     public static void PutKingdomParameterGetter(string pName, Action<Kingdom, Dictionary<string, string>> pGetter)
     {
         kingdom_parameter_getters[pName] = pGetter;
     }
+
     public static void PutCultureParameterGetter(string pName, Action<Culture, Dictionary<string, string>> pGetter)
     {
         culture_parameter_getters[pName] = pGetter;
     }
+
     public static void PutClanParameterGetter(string pName, Action<Clan, Actor, Dictionary<string, string>> pGetter)
     {
         clan_parameter_getters[pName] = pGetter;
     }
+
     public static void PutAllianceParameterGetter(string pName, Action<Alliance, Dictionary<string, string>> pGetter)
     {
         alliance_parameter_getters[pName] = pGetter;
     }
+
     public static void PutWarParameterGetter(string pName, Action<War, Dictionary<string, string>> pGetter)
     {
         war_parameter_getters[pName] = pGetter;
     }
-    public static void PutItemParameterGetter(string pName, Action<ItemData, ItemAsset, Actor, Dictionary<string, string>> pGetter)
+
+    public static void PutItemParameterGetter(string pName,
+        Action<ItemData, ItemAsset, Actor, Dictionary<string, string>> pGetter)
     {
         item_parameter_getters[pName] = pGetter;
     }
+
     public static void PutCustomParameterGetter<T>(string pName, T pGetter) where T : Delegate
     {
         if (!custom_parameter_getters.TryGetValue(typeof(T), out var getters))
@@ -202,6 +228,7 @@ public static class ParameterGetters
             getters = new Dictionary<string, Delegate>();
             custom_parameter_getters[typeof(T)] = getters;
         }
+
         getters[pName] = pGetter;
     }
 }
