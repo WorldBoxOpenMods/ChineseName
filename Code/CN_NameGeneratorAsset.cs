a8eecf410b74934f2b184f436b404dbc22a9a97c
inmny <1342885270@qq.com>
2023-12-06 14:41:54 +0800

    Feature: 提供默认命名模板(当无法在10次尝试内成功生成时)

--- Code/CN_NameGeneratorAsset.cs
+++ Code/CN_NameGeneratorAsset.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Random = UnityEngine.Random;
 
 namespace Chinese_Name;
+
 [Serializable]
 public class CN_NameGeneratorAsset : Asset
 {
-    [JsonProperty("parameter_getter")] public string parameter_getter { get; private set; } = "default";
-
-    [JsonProperty("templates")]
-    public List<CN_NameTemplate> templates { get; private set; }
+    private float total_weight = 0f;
 
     private float[] weights = null;
-    private float total_weight = 0f;
-    
+    [JsonProperty("parameter_getter")] public string parameter_getter { get; private set; } = "default";
+
+    [JsonProperty("default_template")]
+    public CN_NameTemplate default_template { get; private set; } = CN_NameTemplate.Create("#NO_NAME#", 1);
+
+    [JsonProperty("templates")] public List<CN_NameTemplate> templates { get; private set; }
+
     public CN_NameTemplate GetRandomTemplate()
     {
         if (weights == null || weights.Length != templates.Count)
@@ -22,16 +26,19 @@ public class CN_NameGeneratorAsset : Asset
             weights[0] = templates[0].weight;
             for (int i = 1; i < templates.Count; i++)
             {
-                weights[i] = weights[i-1] + templates[i].weight;
+                weights[i] = weights[i - 1] + templates[i].weight;
             }
+
             total_weight = weights[weights.Length - 1];
         }
+
         // 总不能有人写出几十上百个模板吧
-        float random = UnityEngine.Random.Range(0f, total_weight);
+        float random = Random.Range(0f, total_weight);
         for (int i = 0; i < weights.Length; i++)
         {
             if (random < weights[i]) return templates[i];
         }
+
         return templates[templates.Count - 1];
     }
 
@@ -43,6 +50,7 @@ public class CN_NameGeneratorAsset : Asset
             string name = GetRandomTemplate().GenerateName(pParameters);
             if (!string.IsNullOrEmpty(name)) return name;
         }
-        return "NO_NAME";
+
+        return default_template.GenerateName(pParameters);
     }
 }
